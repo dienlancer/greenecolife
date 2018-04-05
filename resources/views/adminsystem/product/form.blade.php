a495b5bf5773ee36d741daa2347f8d959e344abf
trietnk01 <trietnk01@gmail.com>
2018-04-05 15:24:19 +0700

    cập nhật

--- resources/views/adminsystem/product/form.blade.php
+++ resources/views/adminsystem/product/form.blade.php
@@ -19,7 +19,7 @@ $inputIntro            =   '<textarea  name="intro" rows="5" cols="100" class="f
 $inputDetail            =   '<textarea name="detail" rows="5" cols="100" class="form-control" >'.@$arrRowData['detail'].'</textarea>'; 
 $inputSortOrder         =   '<input type="text" class="form-control" name="sort_order"  value="'.@$arrRowData['sort_order'].'">';
 $inputSizeType         =   '<input type="text" class="form-control" name="size_type"     value="'.@$arrRowData['size_type'].'">';
-$ddlCategoryProduct      =   cmsSelectboxCategory("category_id","form-control",$arrCategoryProductRecursive,@$arrRowData['category_id'],"");
+$ddlCategoryProduct      =   cmsSelectboxCategory("category_id","form-control",$arrCategoryProductRecursive,@$arrRowData['category_id'],"",'Chọn danh mục');
 
 $id                     =   (count($arrRowData) > 0) ? @$arrRowData['id'] : "" ;
 $inputID                =   '<input type="hidden" name="id"  value="'.@$id.'" />'; 
